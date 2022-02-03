8f808e4b76511053dafb9e066cd1a00c86991012
hegemon88676 <ff_cris2000@yahoo.com>
2022-02-04 00:46:26 +0200

    added user/register

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,30 @@ import React from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 import "./styles/App.css"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Dashboard from "./components/Dashboard";
 
 export default function App() {
   return (
       <div className="App">
-          <Header />
-          <Meme />
+        {/* <LoginRegister/> */}
+        <Router>
+            <Routes>
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/reset" element={<Reset />} />
+                <Route exact path="/dashboard" element={<Dashboard />} />
+                <Route exact path="/*" 
+                  element={
+                    <div>
+                      <Header/>
+                      <Meme/>
+                    </div>}/>
+            </Routes>
+        </Router>
       </div>
   )
 }
--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,15 +1,51 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
 
 export default function Header() {
+    
+  const [user, loading, error] = useAuthState(auth);
+  const [profileName, setProfileName] = React.useState("")
+
+
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setProfileName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user)   fetchUserName();
+  }, [user, loading]);
+
     return (
         <header className="header">
             <img 
                 src={require("../troll-face.png")}
-                alt="image"
+                alt="Troll face"
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">Login stuff</h4>
+        {user?
+        <h2>
+            Hello <Link to="/dashboard">{profileName}</Link>!
+        </h2>   
+        :
+            <h2>
+                Don't have an account? <Link to="/register">Register</Link> now.
+            </h2>}
         </header>
     )
 }
